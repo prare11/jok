0b757f2441c74974aa2da3359dac1f5771dc4689
prare11 <97444293+prare11@users.noreply.github.com>
2022-01-10 15:31:11 +0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,58 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header';
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1 class="Mark">Mark: responsive, professional PSD template.</h1>
+        Pellentesque vulputate semper ante, non scelerisque arcu consectetur a. Ut et sapien dignissim, malesuada arcu nec, posuere sapien. Integer aliquam sagittis urna, vitae ultrices quam interdum et. Praesent non feugiat ligula. Pellentesque eu maximus orci, a posuere nisl. Duis sollicitudin tristique neque, a ullamcorper justo imperdiet at. Pellentesque aliquet ligula nisl, sit amet facilisis orci iaculis id. Nulla hendrerit molestie magna ac accumsan. Proin nec quam leo. Maecenas cursus facilisis nulla, maximus interdum orci feugiat nec. In fringilla ac ipsum sed condimentum. Etiam facilisis tempor risus, in mattis tortor volutpat nec. In venenatis lacus nisi. Nam maximus lobortis turpis, sed rutrum est rutrum non. Morbi tempor quam urna. Etiam et mi tempor, condimentum libero.
+      <img src="https://images.pexels.com/photos/903516/pexels-photo-903516.jpeg?cs=srgb&dl=pexels-swapnil-sharma-903516.jpg&fm=jpg " class="gog"></img>
+
+      
+      <h1 class="hello">Inruducing Mark</h1>
+      <p class="container">
+      <img
+       src="https://images.pexels.com/photos/69205/pexels-photo-69205.jpeg?cs=srgb&dl=pexels-marianna-69205.jpg&fm=jpg" 
+       class="kro"
+       />
+       <p>
+       Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+       Sed ligula lorem, euismod at sapien sit amet.
+
+       <h4 class="Mark"> Vivamus dapibus in sapien</h4>
+       Donec non magna id eros aliquet ultrices a ut urna.
+       <h4 class="Mark">Donec non lobortis est, ac tristique libero</h4>
+       Sed nec tincidunt mi. Donec aliquam, diam vitae pulvinar pharetra.
+       <h4 class="Mark"> Phasellus ultrices feugiat ipsum</h4>
+       Integer consectetur vel risus nec luctus. Quisque gravida nisi dolor, vel mollis sem gravida eget.
+       </p>
+
+      </p>
+      
+        <h2>An example post</h2>
+        <p class="container">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pulvinar lobortis odio rhoncus laoreet. Maecenas mollis magna eu justo efficitur bibendum Mauris elementum dapibus fringilla. Pellentesque quis tellus dui. Suspendisse potenti. Duis pellentesque elementum tempor. Duis sollicitudin metus ut tellus luctus interdum. Pellentesque nec eros congue erat aliquam maximus.
+        Quisque in rhoncus libero. Aliquam erat volutpat. Suspendisse felis tellus, aliquam nec sem sit amet, varius varius lorem.Phasellus vel molestie nunc.
+        Maecenas quis ultrices purus, non interdum neque. Phasellus hendrerit ultrices eros. Donec mauris tellus, vulputate ut risus at, consectetur dignissim lacus.
+        Curabitur ornare metus eget odio mollis tincidunt.
+        Vivamus porttitor tincidunt consectetur. Cras justo magna, imperdiet quis finibus et, rhoncus id mi. Nulla sed justo ullamcorper, vehicula tortor sed, fringilla sem.
+        Morbi ac iaculis neque, mattis aliquet nibh. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam varius ullamcorper felis a vehicula. Fusce vehicula neque a lacus laoreet aliquam. Donec et sapien at ligula cursus suscipit id id risus. Aenean tincidunt nunc ante, tempus feugiat ex pretium in. Vivamus sagittis risus non blandit sollicitudin. Curabitur mauris diam, euismod ut leo sit amet, hendrerit dapibus felis.
+        
+        <img 
+        src="https://bipbap.ru/wp-content/uploads/2018/06/4635333_original.jpg"
+        class="amm"
+        />
+
+        <img 
+        src="https://bipbap.ru/wp-content/uploads/2018/06/4635333_original.jpg"
+        class="amm"
+        />
+      </p>
+    </div>
+  );
+}
+
+export default App;
